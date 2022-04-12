e0e6f58631c0942c19d4512a7b28c3afa9dc79b9
chathurapereraa <chathura@versustechnologies.io>
2022-04-12 20:54:23 +0530

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../components/header.module.scss";
 import profileImage from "../images/profileImage.png";
-import logo from "../images/logo.png";
 import downArrow from "../images/downArrow.png";
 
 export default function Header() {
---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "../components/header.module.scss";
+
+export default function Search() {
+  return (
+    <div>Search</div>
+  )
+}
