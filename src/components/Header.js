aae4455e7fd8db159898f6e03948f142ed9e6dec
chathurapereraa <chathura@versustechnologies.io>
2022-04-12 16:37:53 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "../components/header.module.scss";
+import profileImage from "../images/profileImage.png";
+import logo from "../images/logo.png";
+import downArrow from "../images/downArrow.png";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerWrap}>
+
+      <div className={styles.logo}>
+          <h1>ONBOARD</h1>
+      </div>
+      <div className={styles.profile}>
+        <img src={profileImage} className={styles.profileImage} alt="" />
+        <p>Chathura perera</p>
+        <img src={downArrow} className={styles.downArrow} alt="" />
+      </div>
+      </div>
+    </header>
+  );
+}
