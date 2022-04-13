1af23b9c1d88702cd516e2be80271ae07469e2fe
chathurperera <chathuraperera007@gmail.com>
2022-04-13 20:03:58 +0530

    icons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import '../src/App.css';
+import Search from './components/Search';
 export default function App() {
   return (
     <div>
-    <Header />  
+    <Header />
+    <div className='main-content'>
+    <Search />  
+    </div>
     </div>
   )
 }
