63eece7c397c0df73efa9feaf33e1723edb9308b
chathurperera <chathuraperera007@gmail.com>
2022-04-21 01:10:24 +0530

    jobs display logic added

--- src/components/JobsWrapper.js
+++ src/components/JobsWrapper.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import styles from '../components/jobsWrapper.module.scss';
 export default function JobsWrapper() {
+    const [expandDetails,setExpandDetails] = useState(false);
     const wrapperStyles = {
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr 1fr',
-    }
+       width: "100%",
+       display:expandDetails ? 'grid' : 'block',
+       gridTemplateColumns:'1fr 2fr',
+       gridGap: '20px' 
 
+    }
+    const expandJobInfo = {
+        display:expandDetails ? 'block' : 'none',
+        width: "100%",
+        border: '1px dashed black'
+    }
+    const jobsContainer = {
+        marginLeft: '20px',
+        display:'grid',
+        gridTemplateColumns:expandDetails ? '1fr' : '1fr 1fr 1fr',
+        gridGap: '20px' 
+
+    }
+    
+
+    
   return (
     <div  className={styles.jobsWrapper} >
         <div className={styles.filter}>
@@ -18,6 +36,13 @@ export default function JobsWrapper() {
         </div>
         <div style={wrapperStyles}>
 
+
+                <div style={jobsContainer} >
+                    <div className={styles.job}><h1>job 1</h1></div>
+                    <div className={styles.job}><h1>job 2</h1></div>
+                    <div className={styles.job}><h1>job 3</h1></div>
+                </div>
+                <div style={expandJobInfo}>More Details</div>
         </div>
     </div>
   )
