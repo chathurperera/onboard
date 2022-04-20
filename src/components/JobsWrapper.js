de5e09903c61ec76cf5ffc36d394345a08aebbc6
chathurperera <chathuraperera007@gmail.com>
2022-04-21 00:10:09 +0530

    job alert box created

--- src/components/JobsWrapper.js
+++ src/components/JobsWrapper.js
@@ -3,16 +3,22 @@ import styles from '../components/jobsWrapper.module.scss';
 export default function JobsWrapper() {
     const wrapperStyles = {
         display: 'grid',
-        gridTemplateColumns: '2fr 8fr',
-        backgroundColor: 'black',
-        width: '90%',
-        maxWidth:  '1700px',
-        margin:'0 auto',
+        gridTemplateColumns: '1fr 1fr 1fr',
     }
 
   return (
-    <div style={wrapperStyles} className={styles.jobsWrapper} >
-        <div></div>
+    <div  className={styles.jobsWrapper} >
+        <div className={styles.filter}>
+            <div className={styles.alertCreate}>
+                <p className={styles.alertTitle}>Create Job Alert</p>
+                <p className={styles.alertDescription}>Create a job alert now and never miss a job</p>
+                <input type="email" name="" id="" />
+                <button>Create Job Alert</button>
+            </div>
+        </div>
+        <div style={wrapperStyles}>
+
+        </div>
     </div>
   )
 }
