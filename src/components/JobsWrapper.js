cd853ff9402f1af36e55a494119ee3ccb3008bb6
chathurperera <chathuraperera007@gmail.com>
2022-04-24 23:22:17 +0530

    job save button added

--- src/components/JobsWrapper.js
+++ src/components/JobsWrapper.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styles from "../components/jobsWrapper.module.scss";
 import companyLogo from '../images/companyLogo.png';
+import save from '../images/save.png';
+import saved from '../images/saved.png';
 
 export default function JobsWrapper() {
   const [expandDetails, setExpandDetails] = useState(false);
@@ -37,11 +39,18 @@ export default function JobsWrapper() {
       <div style={wrapperStyles}>
         <div style={jobsContainer}>
           <div className={styles.job}>
+            <div className={styles.logoWrapper}>
             <img
               className={styles.logo}
               src={companyLogo}
               alt="logo"
             />
+            <img
+              className={styles.save}
+              src={save}
+              alt="logo"
+            />
+            </div>
             <p className={styles.jobTitle}>UI / UX Designer</p>
             <p className={styles.jobDescription}>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
