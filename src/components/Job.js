358bee54d765016f234f1a3fabb610bc86689659
chathurperera <chathuraperera007@gmail.com>
2022-04-24 23:24:51 +0530

    job component created

---
+++ src/components/Job.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from "../components/jobsWrapper.module.scss";
+import companyLogo from '../images/companyLogo.png';
+import save from '../images/save.png';
+import saved from '../images/saved.png';
+
+export default function Job() {
+  return (
+    <div className={styles.job}>
+    <div className={styles.logoWrapper}>
+    <img
+      className={styles.logo}
+      src={companyLogo}
+      alt="logo"
+    />
+    <img
+      className={styles.save}
+      src={save}
+      alt="logo"
+    />
+    </div>
+    <p className={styles.jobTitle}>UI / UX Designer</p>
+    <p className={styles.jobDescription}>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+      Cupiditate minus rerum nulla, nihil voluptatibus praesentium.
+    </p>
+    <div className={styles.jobTags}>
+        <div className={styles.tag}>Full Time</div>
+        <div className={styles.tag}>Remote</div>
+        <div className={styles.tag}>LKR100,00</div>
+    </div>
+    <div className={styles.buttons}>
+        <button>Apply Now</button>
+        <button>Messages</button>
+    </div>
+  </div>
+  )
+}
--- src/components/JobsWrapper.js
+++ src/components/JobsWrapper.js
@@ -3,6 +3,7 @@ import styles from "../components/jobsWrapper.module.scss";
 import companyLogo from '../images/companyLogo.png';
 import save from '../images/save.png';
 import saved from '../images/saved.png';
+import Job from "./Job";
 
 export default function JobsWrapper() {
   const [expandDetails, setExpandDetails] = useState(false);
@@ -38,34 +39,7 @@ export default function JobsWrapper() {
       </div>
       <div style={wrapperStyles}>
         <div style={jobsContainer}>
-          <div className={styles.job}>
-            <div className={styles.logoWrapper}>
-            <img
-              className={styles.logo}
-              src={companyLogo}
-              alt="logo"
-            />
-            <img
-              className={styles.save}
-              src={save}
-              alt="logo"
-            />
-            </div>
-            <p className={styles.jobTitle}>UI / UX Designer</p>
-            <p className={styles.jobDescription}>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Cupiditate minus rerum nulla, nihil voluptatibus praesentium.
-            </p>
-            <div className={styles.jobTags}>
-                <div className={styles.tag}>Full Time</div>
-                <div className={styles.tag}>Remote</div>
-                <div className={styles.tag}>LKR100,00</div>
-            </div>
-            <div className={styles.buttons}>
-                <button>Apply Now</button>
-                <button>Messages</button>
-            </div>
-          </div>
+          <Job/>
           <div className={styles.job}>
             <h1>job 2</h1>
           </div>
