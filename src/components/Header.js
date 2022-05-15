f853a83e5f9fa09693999d141343b0e8a0a25adc
chathurperera <chathuraperera007@gmail.com>
2022-05-16 01:41:39 +0530

    authentication added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../components/header.module.scss";
 import profileImage from "../images/profileImage.png";
 import downArrow from "../images/downArrow.png";
+import userPlacehoder from "../images/user.png";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Header() {
+  const { loginWithRedirect } = useAuth0();
+  const { user, isAuthenticated, logout} = useAuth0();
+  const [login, setLoggedIn] = useState(false);
+  const getAuthStatus = () => {
+    console.log(isAuthenticated)
+  }
   return (
     <header className={styles.header}>
-      <div className={styles.headerWrap} >
-
-      <div className={styles.logo}>
+      <div className={styles.headerWrap}>
+        <div className={styles.logo}>
           <h1>ONBOARD</h1>
-      </div>
-      <div className={styles.profile}>
-        <img src={profileImage} className={styles.profileImage} alt="" />
-        <p>Chathura perera</p>
-        <img src={downArrow} className={styles.downArrow} alt="" />
-      </div>
+        </div>
+        <div className={styles.profile} onClick={() => loginWithRedirect()}>
+          <img
+          onClick={() => getAuthStatus()}
+            src={isAuthenticated ? user.picture : userPlacehoder}
+            className={styles.profileImage}
+            alt=""
+          />
+          {isAuthenticated ? (
+            <button onClick={() => logout({ returnTo: window.location.origin })}>
+              LOGOUT
+            </button>
+          ) : (
+            ""
+          )}
+          {/* <p>Chathura perera</p> */}
+          <img src={downArrow} className={styles.downArrow} alt="" />
+        </div>
       </div>
     </header>
   );
