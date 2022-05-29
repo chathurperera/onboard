165a0ecdfdb0f976ec55001084722b57fc70fcab
chathurperera <chathuraperera007@gmail.com>
2022-05-29 14:37:16 +0530

    sign in page added

---
+++ src/components/LoginHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './LoginHeader.module.scss';
+
+function LoginHeader() {
+  return (
+    <header className={styles.header}>
+            <h1>ONBOARD</h1>
+    </header>
+  )
+}
+
+export default LoginHeader
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import styles from '../pages/login.module.scss';
+import React, { useState } from "react";
+import LoginHeader from "../components/LoginHeader";
+import styles from "../pages/login.module.scss";
+import show from "../images/show.png";
+import hide from "../images/hide.png";
 
-function login() {
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div className={styles.login}>
-        <form >
-            <h1>login</h1>
+      <LoginHeader />
+      <div className={styles.loginWrap}>
+        <form>
+          <h1>Welcome Back !</h1>
+          <div className={styles.inputBox}>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div className={styles.inputBox}>
+            <img
+              src={showPassword ? show : hide}
+              alt="show icon"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+            <label>Password</label>
+            <input type={showPassword ? "text" : "password"} />
+          </div>
+          <button>Sign in</button>
+          <p>
+            Forgot password? <span>Reset Here</span>
+          </p>
+          <p className={styles.register}>
+            New to ONBORAD? <span>Register Now</span>
+          </p>
         </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default login
+export default Login;
