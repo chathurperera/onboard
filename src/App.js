58525ce870cbbc99619a2e2d562daf43191e4453
chathurperera <chathuraperera007@gmail.com>
2022-05-29 14:55:46 +0530

    register page added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import "../src/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          
-          <Route path="login" element={<Login />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Register.js
@@ -0,0 +1,51 @@
+import React , { useState } from "react";
+import LoginHeader from "../components/LoginHeader";
+import styles from "../pages/Register.module.scss";
+import show from "../images/show.png";
+import hide from "../images/hide.png";
+
+
+function Register() {
+    const [showPassword, setShowPassword] = useState(false);
+ 
+    return (
+    <div className={styles.register}>
+      <LoginHeader />
+      <div className={styles.registerWrap}>
+        <form>
+          <h1>Let's get started</h1>
+          <div className={styles.inputWrap}>
+            <div className={styles.inputBox}>
+              <label>First name</label>
+              <input type="text" />
+            </div>
+            <div className={styles.inputBox}>
+              <label>Last name</label>
+              <input type="text" />
+            </div>
+          </div>
+          <div className={styles.inputBox}>
+            <label>Email address</label>
+            <input type="email" />
+          </div>
+          <div className={styles.inputBox}>
+            <label>Phone</label>
+            <input type="tel" />
+          </div>
+          <div className={styles.inputBox}>
+            <img
+              src={showPassword ? show : hide}
+              alt="show icon"
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+            <label>Password</label>
+            <input type={showPassword ? "text" : "password"} />
+          </div>
+          <button>Create Profile</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
