8948a737aee5f5ed216817eb5997c81eaf112b7e
chathurperera <chathuraperera007@gmail.com>
2022-06-22 13:20:20 +0530

    login validations added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import styles from "../components/header.module.scss";
 import profileImage from "../images/profileImage.png";
+import userNoPic from "../images/userNoPic.png";
 import downArrow from "../images/downArrow.png";
 import signOut from "../images/sign-out.png";
 import userPlaceholder from "../images/user.png";
 import { useAuth0 } from "@auth0/auth0-react";
 import DropDownPanel from "../components/home/DropDownCard";
-import { NavLink, useLocation , } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 
@@ -23,9 +24,9 @@ export const Header = () => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
-        setName(localStorage.getItem("name"));
-        setEmail(localStorage.getItem("email"));
-        setProfilePic(localStorage.getItem("profilePic"));
+        setName(user.displayName);
+        setEmail(user.email);
+        setProfilePic(user.photoURL);
       } else {
         setLoggedIn(false);
       }
@@ -63,20 +64,31 @@ export const Header = () => {
             <>
               <img
                 className={styles.profileImage}
-                src={profilePic}
+                src={profilePic ? profilePic : userNoPic}
                 alt={name}
               />
-              <h2 className={styles.userName}>{name}</h2>
+              <h2
+                className={styles.userName}
+                onClick={() => setExpandPanel(!expandPanel)}
+              >
+                {name ? name : email}
+              </h2>
+              <img
+                src={downArrow}
+                className={expandPanel ? styles.upArrow : styles.downArrow}
+                alt=""
+                onClick={() => setExpandPanel(!expandPanel)}
+              />
             </>
           )}
-          {LoggedIn && (
+          {/* {LoggedIn && (
             <img
               src={downArrow}
               className={expandPanel ? styles.upArrow : styles.downArrow}
               alt=""
               onClick={() => setExpandPanel(!expandPanel)}
             />
-          )}
+          )} */}
         </div>
         {expandPanel && <DropDownPanel />}
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import LoginHeader from "../components/login/LoginHeader";
 import styles from "../pages/login.module.scss";
 import show from "../images/show.png";
 import hide from "../images/hide.png";
+import spinner from "../images/Spinner.svg";
 import google from "../images/google.png";
 import {
   GoogleAuthProvider,
@@ -10,12 +11,12 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../firebase-config";
-import { useNavigate , Link } from "react-router-dom";
-
+import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
-  
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [loginFailed, setLoginFailed] = useState(false);
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
   const navigate = useNavigate();
@@ -37,14 +38,19 @@ function Login() {
 
   const login = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const user = await signInWithEmailAndPassword(
         auth,
         loginEmail,
         loginPassword
       );
+      setLoading(false);
+      navigate("/");
       console.log("user", user);
     } catch (error) {
+      setLoading(false);
+      setLoginFailed(true);
       console.log(error.message);
     }
   };
@@ -58,25 +64,43 @@ function Login() {
         <form>
           <h1>Welcome Back !</h1>
           <div className={styles.inputBox}>
+          <div className={styles.inputBox}>
             <label>Email</label>
             <input
+              className={styles.input}
               type="email"
               onChange={(e) => setLoginEmail(e.target.value)}
             />
           </div>
-          <div className={styles.inputBox}>
-            <img
-              src={showPassword ? show : hide}
-              alt="show icon"
-              onClick={() => setShowPassword((prevState) => !prevState)}
-            />
-            <label>Password</label>
-            <input
-              onChange={(e) => setLoginPassword(e.target.value)}
-              type={showPassword ? "text" : "password"}
-            />
+            {loginFailed && (
+              <span className={styles.authError}>
+                Please check your credentials
+              </span>
+            )}
           </div>
-          <button onClick={(e) => login(e)}>Sign in</button>
+          <div className={styles.inputBox}>
+            <div className={styles.inputBox}>
+              <img
+                src={showPassword ? show : hide}
+                alt="show icon"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+              <label>Password</label>
+              <input
+                className={styles.input}
+                onChange={(e) => setLoginPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+              />
+            </div>
+            {loginFailed && (
+              <span className={styles.authError}>
+                Please check your credentials
+              </span>
+            )}
+          </div>
+          <button onClick={(e) => login(e)} disabled={loading}>
+            {loading ? <img src={spinner} alt="spinner" width={25} /> : "Sign in"}
+          </button>
           <div className={styles.googleLogin} onClick={signInWithGoogle}>
             <img src={google} alt="google" />
             <p>Sign in with Google</p>
@@ -85,7 +109,11 @@ function Login() {
             Forgot password? <span>Reset Here</span>
           </p>
           <p className={styles.register}>
-            New to ONBORAD? <span> <Link to='/Register'>Register Now</Link>  </span>
+            New to ONBORAD?{" "}
+            <span>
+              {" "}
+              <Link to="/Register">Register Now</Link>{" "}
+            </span>
           </p>
         </form>
       </div>
