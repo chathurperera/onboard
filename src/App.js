f822bfae784ec25669188d944202460ede432404
chathurperera <chathuraperera007@gmail.com>
2022-06-27 16:39:56 +0530

    header navlinks added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import SharedLayout from "./pages/SharedLayout";
+import Requests from "./pages/Requests";
+
+import { Applications } from "./pages/Applications";
+import { Profile } from "./pages/Profile";
 export default function App() {
   return (
     <div>
@@ -12,6 +16,9 @@ export default function App() {
         <Routes>
           <Route path="/" element={<SharedLayout />}>
             <Route path="/" element={<HomePage />} />
+            <Route path="/requests" element={<Requests />} />
+            <Route path="/Applications" element={<Applications />} />
+            <Route path="/Profile" element={<Profile />} />
           </Route>
             <Route path="Login" element={<Login />} />
             <Route path="Register" element={<Register />} />
