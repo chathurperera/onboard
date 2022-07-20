84dd2d9784709b925ad64bcea0bfa9ae1ebb096b
chathurperera <chathuraperera007@gmail.com>
2022-07-21 00:48:37 +0530

    basic info and external links section added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,38 +4,74 @@ import close from "../images/close (2).png";
 import jobRoles from "../data.json";
 
 const Profile = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [openToRoles, setOpenToRoles] = useState([]);
 
   const addRole = (newRole) => {
     setOpenToRoles((prevRoles) => [...prevRoles, newRole]);
-    setSearchQuery('');
-  }
+    setSearchQuery("");
+  };
   return (
     <main className={styles.container}>
-    <div className={styles.profile}>
-      <div className={styles.detailsSection}>
-        <h3 className={styles.detailsTitle}>General</h3>
-        <div className={styles.inputBox}>
-          <label>Where do you live?</label>
-          <input type="text" />
+      <div className={styles.profile}>
+        <div>
+          <div className={styles.detailsSection}>
+            <div className={styles.cardHeader}>BASIC INFO</div>
+            <div className={styles.detailsWrapper}>
+              <div className="right">
+                <div className={styles.inputBox}>
+                  <label htmlFor="">First Name</label>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Last Name</label>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+              <div className="right">
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Email</label>
+                  <input type="email" name="" id="" />
+                </div>
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Phone Number</label>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+              <div className={`${styles.inputBox} ${styles.fullWidth}`}>
+                <label htmlFor="">Address</label>
+                <input type="text" name="" id="" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.detailsSection}>
+            <div className={styles.cardHeader}>EXTERNAL LINKS</div>
+            <div className={styles.detailsWrapper}>
+              <div className="right">
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Linkedin</label>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Github</label>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+              <div className="right">
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Portfolio</label>
+                  <input type="text" name="" id="" />
+                </div>
+                <div className={styles.inputBox}>
+                  <label htmlFor="">Codersrank</label>
+                  <input type="text" name="" id="" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.inputBox}>
-          <label>Your Primary Role</label>
-          <input type="text" />
-        </div>
-        <div className={styles.inputBox}>
-        <label>Summary</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
-        <div className={styles.btnwrap}>
-          <button>Save </button>
-          <button>Cancel </button>
-        </div>
+        <div className={styles.skillsSection}></div>
       </div>
-      <div className={styles.skillsSection}></div>
-    </div>
-    
     </main>
   );
 };
